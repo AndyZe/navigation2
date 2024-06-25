33c16bb4b0466ad25d55c0983478da307bd9a418
AndyZe <andyz@utexas.edu>
2024-06-24 22:03:25 -0500

    Add a BT plugin to check robot vicinity with ML model

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/ml_vicinity_condition.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/condition/ml_vicinity_condition.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Joshua Wallace
+// Copyright (c) 2024 Andy Zelenak
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,36 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_VALID_CONDITION_HPP_
-#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_VALID_CONDITION_HPP_
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ML_VICINITY_CONDITION_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ML_VICINITY_CONDITION_
 
 #include <string>
-#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp_v3/condition_node.h"
-#include "nav2_msgs/srv/is_path_valid.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 namespace nav2_behavior_tree
 {
 
 /**
- * @brief A BT::ConditionNode that returns SUCCESS when the IsPathValid
- * service returns true and FAILURE otherwise
+ * @brief A BT::ConditionNode that returns SUCCESS when a large language model says that a path is clear
+ *        The input data has type sensor_msgs::msg::Image.
  */
-class IsPathValidCondition : public BT::ConditionNode
+class MLVicinityCondition : public BT::ConditionNode
 {
 public:
   /**
-   * @brief A constructor for nav2_behavior_tree::IsPathValidCondition
+   * @brief A constructor for nav2_behavior_tree::MLVicinityCondition
    * @param condition_name Name for the XML tag for this node
    * @param conf BT node configuration
    */
-  IsPathValidCondition(
+  MLVicinityCondition(
     const std::string & condition_name,
     const BT::NodeConfiguration & conf);
 
-  IsPathValidCondition() = delete;
+  MLVicinityCondition() = delete;
 
   /**
    * @brief The main override required by a BT action
@@ -56,19 +55,39 @@ public:
   static BT::PortsList providedPorts()
   {
     return {
-      BT::InputPort<nav_msgs::msg::Path>("path", "Path to Check"),
+      BT::InputPort<std::string>("image_topic", "Image topic which is subscribed to"),
       BT::InputPort<std::chrono::milliseconds>("server_timeout")
     };
   }
 
 private:
+  /**
+   * @brief Capture the latest image to send to the ML model
+   * 
+   * @param msg 
+   */
+  void imageCallback(const sensor_msgs::msg::Image& msg)
+  {
+    const std::lock_guard<std::mutex> lock(image_mutex_);
+    latest_image_ = msg;
+  }
+
+  /**
+  * @brief Send the prompt (or otherwise run) the AI model. This includes the latest image as input.
+  * 
+  * @return true if successful
+  */
+  [[nodiscard]] bool promptAIModel();
+
   rclcpp::Node::SharedPtr node_;
-  rclcpp::Client<nav2_msgs::srv::IsPathValid>::SharedPtr client_;
   // The timeout value while waiting for a response from the
   // is path valid service
   std::chrono::milliseconds server_timeout_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+  mutable std::mutex image_mutex_;
+  std::optional<sensor_msgs::msg::Image> latest_image_;
 };
 
 }  // namespace nav2_behavior_tree
 
-#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__IS_PATH_VALID_CONDITION_HPP_
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__CONDITION__ML_VICINITY_CONDITION_
